7f112212038dd7a85c04aa69b27162d8761ed291
stefanpricopie <stefan.pricopie@postgrad.manchester.ac.uk>
2023-07-10 18:48:26 +0100

    _tell added from skopt

--- ercopt.py
+++ ercopt.py
@@ -1,6 +1,12 @@
 import warnings
 
+from joblib import Parallel, delayed
+import numpy as np
+from scipy.optimize import fmin_l_bfgs_b
+from sklearn.base import clone
 from skopt import Optimizer as skopt_Optimizer
+from skopt.acquisition import _gaussian_acquisition, gaussian_acquisition_1D
+from skopt.utils import is_listlike, is_2Dlistlike
 
 class Optimizer(skopt_Optimizer):
     def __init__(self, erc, strategy, *args, **kwargs):
@@ -56,3 +62,121 @@ class Optimizer(skopt_Optimizer):
 
         # return point computed from last call to tell()
         return next_x
+    
+    def _tell(self, x, y, fit=True):
+        """Perform the actual work of incorporating one or more new points.
+        See `tell()` for the full description.
+
+        This method exists to give access to the internals of adding points
+        by side stepping all input validation and transformation."""
+
+        if "ps" in self.acq_func:
+            if is_2Dlistlike(x):
+                self.Xi.extend(x)
+                self.yi.extend(y)
+                self._n_initial_points -= len(y)
+            elif is_listlike(x):
+                self.Xi.append(x)
+                self.yi.append(y)
+                self._n_initial_points -= 1
+        # if y isn't a scalar it means we have been handed a batch of points
+        elif is_listlike(y) and is_2Dlistlike(x):
+            self.Xi.extend(x)
+            self.yi.extend(y)
+            self._n_initial_points -= len(y)
+        elif is_listlike(x):
+            self.Xi.append(x)
+            self.yi.append(y)
+            self._n_initial_points -= 1
+        else:
+            raise ValueError("Type of arguments `x` (%s) and `y` (%s) "
+                             "not compatible." % (type(x), type(y)))
+
+        # optimizer learned something new - discard cache
+        self.cache_ = {}
+
+        # after being "told" n_initial_points we switch from sampling
+        # random points to using a surrogate model
+        if (fit and self._n_initial_points <= 0 and
+                self.base_estimator_ is not None):
+            transformed_bounds = np.array(self.space.transformed_bounds)
+            est = clone(self.base_estimator_)
+
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                est.fit(self.space.transform(self.Xi), self.yi)
+
+            if hasattr(self, "next_xs_") and self.acq_func == "gp_hedge":
+                self.gains_ -= est.predict(np.vstack(self.next_xs_))
+
+            if self.max_model_queue_size is None:
+                self.models.append(est)
+            elif len(self.models) < self.max_model_queue_size:
+                self.models.append(est)
+            else:
+                # Maximum list size obtained, remove oldest model.
+                self.models.pop(0)
+                self.models.append(est)
+
+            # even with BFGS as optimizer we want to sample a large number
+            # of points and then pick the best ones as starting points
+            X = self.space.transform(self.space.rvs(
+                n_samples=self.n_points, random_state=self.rng))
+
+            self.next_xs_ = []
+            for cand_acq_func in self.cand_acq_funcs_:
+                values = _gaussian_acquisition(
+                    X=X, model=est, y_opt=np.min(self.yi),
+                    acq_func=cand_acq_func,
+                    acq_func_kwargs=self.acq_func_kwargs)
+                # Find the minimum of the acquisition function by randomly
+                # sampling points from the space
+                if self.acq_optimizer == "sampling":
+                    next_x = X[np.argmin(values)]
+
+                # Use BFGS to find the mimimum of the acquisition function, the
+                # minimization starts from `n_restarts_optimizer` different
+                # points and the best minimum is used
+                elif self.acq_optimizer == "lbfgs":
+                    x0 = X[np.argsort(values)[:self.n_restarts_optimizer]]
+
+                    with warnings.catch_warnings():
+                        warnings.simplefilter("ignore")
+                        results = Parallel(n_jobs=self.n_jobs)(
+                            delayed(fmin_l_bfgs_b)(
+                                gaussian_acquisition_1D, x,
+                                args=(est, np.min(self.yi), cand_acq_func,
+                                      self.acq_func_kwargs),
+                                bounds=self.space.transformed_bounds,
+                                approx_grad=False,
+                                maxiter=20)
+                            for x in x0)
+
+                    cand_xs = np.array([r[0] for r in results])
+                    cand_acqs = np.array([r[1] for r in results])
+                    next_x = cand_xs[np.argmin(cand_acqs)]
+
+                # lbfgs should handle this but just in case there are
+                # precision errors.
+                if not self.space.is_categorical:
+                    next_x = np.clip(
+                        next_x, transformed_bounds[:, 0],
+                        transformed_bounds[:, 1])
+                self.next_xs_.append(next_x)
+
+            if self.acq_func == "gp_hedge":
+                logits = np.array(self.gains_)
+                logits -= np.max(logits)
+                exp_logits = np.exp(self.eta * logits)
+                probs = exp_logits / np.sum(exp_logits)
+                next_x = self.next_xs_[np.argmax(self.rng.multinomial(1,
+                                                                      probs))]
+            else:
+                next_x = self.next_xs_[0]
+
+            # note the need for [0] at the end
+            self._next_x = self.space.inverse_transform(
+                next_x.reshape((1, -1)))[0]
+
+        # Pack and return results
+        return self.get_result()
